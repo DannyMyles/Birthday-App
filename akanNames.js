8c1115d8bfc25b53b33b866c879296dafdeb193b
Moringa <dancun.wawire@student.moringaschool.com>
2021-09-04 20:29:04 +0300

    edited

---
+++ akanNames.js
@@ -0,0 +1,58 @@
+const akanNames = [
+    {
+        gender:'Male',
+        dayOfBirth: 'Sunday',
+        name:'Kwasi'
+    },
+    {
+        gender:'Male',
+        dayOfBirth: 'Wednesday',
+        name:'Kwaku'
+    },
+    {
+        gender:'Male',
+        dayOfBirth: 'Thursday',
+        name:'Yaw'
+    },
+    {
+        gender:'Male',
+        dayOfBirth: 'Friday',
+        name:'Kofi'
+    },
+    {
+        gender:'Male',
+        dayOfBirth: 'Saturday',
+        name:'Kwame'
+    },{
+        gender:'Female',
+        dayOfBirth: 'Sunday',
+        name:'Akosua'
+    }, {
+        gender:'Female',
+        dayOfBirth: 'Wednesday',
+        name:'Adwoa'
+    },
+    {
+        gender:'Female',
+        dayOfBirth: 'Thursday',
+        name:'Abenaa'
+    },
+    {
+        gender:'Female',
+        dayOfBirth: 'Friday',
+        name:'Akua'
+    },
+    {
+        gender:'Female',
+        dayOfBirth: 'Saturday',
+        name:'Yaa'
+    },{
+        gender:'Female',
+        dayOfBirth: 'Saturday',
+        name:'Afua'
+    },{
+        gender:'Female',
+        dayOfBirth: 'Saturday',
+        name:'Ama'
+    }
+];
---
+++ main.js
@@ -0,0 +1,30 @@
+
+/* const dateOfBirthElement = document.getElementById('date');
+const genderElement = document.getElementById('gender'); */
+const akansForm = document.getElementById('akansForm');
+const daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const calcBtn = document.getElementById('calculateButton');
+const result = document.getElementById('result');
+
+function calculateDayOfBirth(birthDate, gender){
+
+    const dateOfBirth = new Date(birthDate);
+    const dayOfTheWeek = dateOfBirth.getDay();
+
+    const dayOfBirth = daysOfTheWeek[dayOfTheWeek];
+
+    return akanNames.find(function(akan){
+        return akan.dayOfBirth == dayOfBirth && akan.gender.toLowerCase() == gender.toLowerCase();
+    });
+
+}
+
+calcBtn.addEventListener('click', function(){
+    const akan = calculateDayOfBirth(akansForm.date.value, akansForm.gender.value );
+    result.innerHTML = akan.name;
+    // alert(akan.name);
+})
+
+
+
+
