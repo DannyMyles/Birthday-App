4903bfa5eda11a9984542b7aec59dbaae6f15cc2
Moringa <dancun.wawire@student.moringaschool.com>
2021-09-15 12:08:13 +0300

    modified

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
 
+/* const dateOfBirthElement = document.getElementById('date');
+const genderElement = document.getElementById('gender'); */
 const akansForm = document.getElementById('akansForm');
 const daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const calcBtn = document.getElementById('calculateButton');
 const result = document.getElementById('result');
 
 function calculateDayOfBirth(birthDate, gender){
-    /**
-     * hapa pia check if birthDate and gender are defined
-     * if(!birthDate || !gender){
-     *  return false;
-     * }
-     *  */
-    
-    const dateOfBirth = new Date(birthDate);
-    const dayOfTheWeek = dateOfBirth.getDay();
 
-    const dayOfBirth = daysOfTheWeek[dayOfTheWeek];
+    const dateOfBirth = new Date(birthDate);
+    
+    const CC = dateOfBirth.getFullYear().toString().substr(0, 2);
+    const YY = dateOfBirth.getFullYear().toString().substr(2, 4);
+    const MM = dateOfBirth.getMonth()+1;
+    const DD = dateOfBirth.getDate();
+    
+    const dayOfTheWeek = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
+    
+
+    const dayOfBirth = daysOfTheWeek[Math.floor(dayOfTheWeek)];
+
 
     return akanNames.find(function(akan){
         return akan.dayOfBirth == dayOfBirth && akan.gender.toLowerCase() == gender.toLowerCase();
